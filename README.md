23aa8b1522d52630f645f74dca217d36d042541f
Exepp <pavel.glomski@gmail.com>
2020-09-23 22:43:18 +0200

    Setting up virual environment and basic required packages

---
+++ README.md
@@ -0,0 +1,1 @@
+Link to a nice tutorial (start from 10 episode): https://www.youtube.com/playlist?list=PL-wATfeyAMNrtbkCNsLcpoAyBBRJZVlnf
---
+++ makeEnv.sh
@@ -0,0 +1,2 @@
+mkdir venv && sudo apt update && sudo apt install python3.6 && sudo apt install virtualenv && \
+    virtualenv -p `which python3.6` ./venv/ && source venv/bin/activate && pip install -r requirements.txt
