c943730e04c8b1052a80eec9f1c03b2177850b97
Exepp <pavel.glomski@gmail.com>
2020-12-01 20:50:05 +0100

    Current version

--- src/Cutter.py
+++ src/Cutter.py
@@ -10,77 +10,52 @@ import GenerateDataset as gd
 import subprocess
 import pickle
 import shutil
+import librosa
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+from GenerateDataset import SEG_CELLS
+
 TO_OPTIMIZE_DIR = (Path.cwd()/("../toOptimize" if Path.cwd().name == "src" else "toOptimize")).resolve()
 V_FPS = 15
-THRESHOLD = 0.77
+THRESHOLD = 0.60
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
 
 
-def loadDataset(validationRatio):
+def loadDataset():
     dataset = pickle.load(dsFile)
-    dataset["data"] = (dataset["data"].astype(np.float32) / np.iinfo(dataset["data"].dtype).max)[..., np.newaxis]
+    dataset["data"] = dataset["data"][..., np.newaxis]
     temp = dataset["range"][0]
     return (dataset, (temp[1] - temp[0], *dataset["data"].shape[1:]), dataset["label"][0].shape)
 
 
 def makeModel(inputShape, outputShape):
     # xIn = keras.Input(shape=inputShape)
-    # x = layers.BatchNormalization()(xIn)
-    # x = layers.Conv1D(256, kernel_size=17, strides=8, activation="relu", padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # xDil1 = layers.Conv1D(32, kernel_size=3, dilation_rate=1, activation="relu",
-    #                       padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # xDil2 = layers.Conv1D(32, kernel_size=3, dilation_rate=3, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil1)
-    # xDil3 = layers.Conv1D(32, kernel_size=3, dilation_rate=9, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil2)
-    # xDil4 = layers.Conv1D(32, kernel_size=3, dilation_rate=27, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil3)
-    # x = layers.Concatenate()([xDil1, xDil2, xDil3, xDil4, x])
-    # x = layers.BatchNormalization()(x)
 
-    # x = layers.Conv1D(256, kernel_size=9, strides=4, activation="relu", padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # xDil1 = layers.Conv1D(32, kernel_size=3, dilation_rate=1, activation="relu",
-    #                       padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # xDil2 = layers.Conv1D(32, kernel_size=3, dilation_rate=3, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil1)
-    # xDil3 = layers.Conv1D(32, kernel_size=3, dilation_rate=9, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil2)
-    # xDil4 = layers.Conv1D(32, kernel_size=3, dilation_rate=27, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil3)
-    # x = layers.Concatenate()([xDil1, xDil2, xDil3, xDil4, x])
-    # x = layers.BatchNormalization()(x)
+    # x = keras.layers.Conv2D(160, kernel_size=(3, 3), strides=(2, 3), activation="elu")(xIn)
+    # x = keras.layers.MaxPool2D((2, 2))(x)
 
-    # x = layers.Conv1D(256, kernel_size=5, strides=2, activation="relu", padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # xDil1 = layers.Conv1D(32, kernel_size=3, dilation_rate=1, activation="relu",
-    #                       padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # xDil2 = layers.Conv1D(32, kernel_size=3, dilation_rate=3, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil1)
-    # xDil3 = layers.Conv1D(32, kernel_size=3, dilation_rate=9, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil2)
-    # xDil4 = layers.Conv1D(32, kernel_size=3, dilation_rate=27, activation="relu", padding="same",
-    #                       kernel_regularizer=tf.keras.regularizers.L2(0.001))(xDil3)
-    # x = layers.Concatenate()([xDil1, xDil2, xDil3, xDil4, x])
-    # x = layers.BatchNormalization()(x)
+    # x = keras.layers.Conv2D(256, kernel_size=(2, 2), activation="elu")(x)
+    # x = keras.layers.MaxPool2D((2, 2))(x)
 
-    # x = layers.Conv1D(256, kernel_size=5, strides=2, activation="relu", padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # x = layers.Conv1D(256, kernel_size=3, strides=2, activation="relu", padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # x = layers.Conv1D(512, kernel_size=3, strides=2, activation="relu", padding="same", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
+    # x = keras.layers.Conv2D(256+128, kernel_size=(2, 2), activation="elu")(x)
+    # x = keras.layers.MaxPool2D((2, 2))(x)
+    # x = keras.layers.Conv2D(512+256, kernel_size=(2, 2), activation="elu")(x)
+    # x = keras.layers.MaxPool2D((2, 2))(x)
     # x = layers.Flatten()(x)
-    # x = layers.Dense(256, activation="relu", kernel_regularizer=tf.keras.regularizers.L2(0.001))(x)
-    # x = layers.Dropout(0.2)(x)
+    # x = layers.Dropout(0.5)(x)
+
+    # x = layers.Dense(150, activation="elu")(x)
+    # x = layers.Dropout(0.4)(x)
     # xOut = layers.Dense(outputShape[0], activation="sigmoid")(x)
 
     # model = keras.Model(inputs=xIn, outputs=xOut)
     # model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
-    #               loss='binary_crossentropy',
-    #               metrics=['accuracy'])
+    #               loss='binary_crossentropy')
     model = keras.models.load_model("lastModel")
     model.summary()
     return model
@@ -94,7 +69,7 @@ def prepareBatch(batch, dataset):
     return np.array([dataset["data"][segRange[0]:segRange[1]] for segRange in batch])
 
 
-def fit(model, dataset, batchSize, epochs, validationPart=0.2):
+def fit(model, dataset, batchSize, epochs, validationPart=0.1):
     x, vX, y, vY = splitDataset(dataset, validationPart)
 
     epochLen = int(x.shape[0] / batchSize)
@@ -104,55 +79,45 @@ def fit(model, dataset, batchSize, epochs, validationPart=0.2):
         for i, trainIdcs in enumerate(np.random.permutation(epochLen * batchSize).reshape(epochLen, batchSize)):
             trainX = prepareBatch(np.take(x, trainIdcs, axis=0), dataset)
             trainY = np.take(y, trainIdcs, axis=0)
-            trLoss, trAcc = model.train_on_batch(trainX, trainY, reset_metrics=False)
-            print(f"Epoch #{e+1}: {i / epochLen * 100:.2f}%: trAcc = {trAcc:.4f}, trLoss = {trLoss:.4f}\r", end="", flush=True)
+            trLoss = model.train_on_batch(trainX, trainY, reset_metrics=False)
+            print(f"Epoch #{e+1}: {i / epochLen * 100:.2f}%: trLoss = {trLoss:.4f}\r", end="", flush=True)
         model.reset_metrics()
         # validate
         for i, valIdcs in enumerate(np.random.permutation(validLen * batchSize).reshape(validLen, batchSize)):
             valX = prepareBatch(np.take(vX, valIdcs, axis=0), dataset)
             valY = np.take(vY, valIdcs, axis=0)
-            vaLoss, vaAcc = model.test_on_batch(valX, valY, reset_metrics=False)
+            vaLoss = model.test_on_batch(valX, valY, reset_metrics=False)
         model.reset_metrics()
 
-        print(f"Epoch #{e+1}: Done! trAcc = {trAcc:.4f}, trLoss = {trLoss:.4f}, vaAcc = {vaAcc:.4f}, vaLoss = {vaLoss:.4f}")
+        print(f"Epoch #{e+1}: Done! trLoss = {trLoss:.4f}, vaLoss = {vaLoss:.4f}")
 
         if e % 5 == 4:
             model.save("lastModel")
 
 
 with open(gd.DATASET_PATH, "rb") as dsFile:
-    dataset, inShape, outShape = loadDataset(0.2)
-    model = makeModel(inShape, outShape)
-    fit(model, dataset, 32, 60)
-    model.save("lastModel")
+    # dataset, inShape, outShape = loadDataset()
+    # model = makeModel(inShape, outShape)
+    # fit(model, dataset, 32, 100)
+    # model.save("lastModel")
 
     model = keras.models.load_model("lastModel")
     for fileName in TO_OPTIMIZE_DIR.glob("*"):
         if not Path(fileName).is_file():
             continue
         sig = gd.loadSignal(fileName)
-        sig = sig[:int(len(sig) / gd.SEG_LEN) * gd.SEG_LEN] / np.iinfo(sig.dtype).max
+        sig = sig[:int(len(sig) / gd.SEG_LEN) * gd.SEG_LEN]
+        melIn = gd.calcMelSpect(sig)
 
         labels = np.zeros(int(len(sig) / gd.SEG_CELL_LEN))
-        for i, offset in enumerate(range(0, gd.SEG_LEN, int(gd.SEG_LEN/4))):
-            tempSig = np.concatenate([sig[offset:], np.zeros(offset)]).reshape((-1, gd.SEG_LEN, 1))
-            cellIdx = int(offset/gd.SEG_CELL_LEN)
-            labels[cellIdx:] *= i
-            labels[cellIdx:] += model.predict(tempSig, verbose=True).reshape(-1)[:len(labels)-cellIdx]
-            labels[cellIdx:] /= i+1
-        labels = labels >= THRESHOLD
-
-        # with open("labels.save", "wb") as lf:
-        #     pickle.dump(labels, lf)
-        # with open("labels.save", "rb") as lf:
-        #     labels = pickle.load(lf)
-
-        # for tresh in np.linspace(0.2, 0.9, 30):
-        #     tmp = labels >= tresh
-        #     for i in range(1, len(tmp)-1):
-        #         tmp[i] = (tmp[i-1] and tmp[i+1]) or (tmp[i] and (tmp[i-1] or tmp[i+1]))  # remove singular (alone) cells
-        #     audio = sig.reshape((-1, gd.SEG_CELL_LEN))[tmp].reshape(-1)
-        #     sf.write(f"test_{tresh:.2f}.wav", audio, gd.SAMPLE_RATE)
+        for i, offset in enumerate(range(0, gd.SEG_CELLS, int(np.ceil(gd.SEG_CELLS/3)))):
+            realPart = melIn[offset:]
+            compPart = np.zeros((offset, melIn.shape[1]))
+            tempMel = np.concatenate([realPart, compPart]).reshape((-1, gd.SEG_CELLS, melIn.shape[1], 1))
+            labels[offset:] *= i
+            labels[offset:] += model.predict(tempMel, verbose=True).reshape(-1)[:len(labels)-offset] >= THRESHOLD
+            labels[offset:] /= i+1
+        labels = labels >= 2/3
 
         for i in range(1, len(labels)-1):
             labels[i] = (labels[i-1] and labels[i+1]) or (labels[i] and (labels[i-1] or labels[i+1]))  # remove singular (alone) cells
@@ -164,6 +129,10 @@ with open(gd.DATASET_PATH, "rb") as dsFile:
         if (isEven and labels[0]) or (not isEven and not labels[0]):
             ranges = np.concatenate([ranges, [len(labels)]])
         ranges = ranges.reshape((-1, 2))
+
+        # with open(f"optimized/{fileName.stem}.txt", "w") as file:
+        #     for r in ranges * (gd.SEG_CELL_LEN / gd.SAMPLE_RATE):
+        #         file.write(f"{r[0]}\t{r[1]}\n")
 
         framesToGet = []
         videoLen = 0
@@ -191,12 +160,11 @@ with open(gd.DATASET_PATH, "rb") as dsFile:
 
         p = subprocess.Popen([f"ffmpeg -y -i {str(fileName)} -an -vsync cfr -r {V_FPS} " +
                               f"-c:v hevc_nvenc -preset fast .temp/norm.mp4"], shell=True)
-
         audio = sig.reshape((-1, gd.SEG_CELL_LEN))[labels].reshape(-1)
         sf.write(f".temp/audio.wav", audio, gd.SAMPLE_RATE)
         p.wait()
 
         subprocess.Popen([f"ffmpeg -y -i .temp/norm.mp4 -i .temp/audio.wav -c:v libx265 -preset ultrafast -filter_script:v .temp/vf " +
-                          f"-c:a aac -map 0:v:0 -map 1:a:0 -vsync cfr -r {V_FPS} optimized/{fileName.stem}_{THRESHOLD}_optimized.mp4"], shell=True).wait()
+                          f"-c:a aac -map 0:v:0 -map 1:a:0 -vsync cfr -r {V_FPS} optimized/{fileName.stem}_optimized.mp4"], shell=True).wait()
 
     shutil.rmtree(".temp/")
--- src/GenerateDataset.py
+++ src/GenerateDataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import ffmpeg
 import csv
 import pickle
+import librosa
 
 DATA_DIR = (Path.cwd()/("../data" if Path.cwd().name == "src" else "data")).resolve()
 DATASET_PATH = DATA_DIR/"dataset"
@@ -11,16 +12,23 @@ SAMPLE_RATE = 16000
 SEG_LEN = int(1.0*SAMPLE_RATE)
 SEG_CELL_LEN = 128
 SEG_CELLS = int(SEG_LEN / SEG_CELL_LEN) if SEG_LEN % SEG_CELL_LEN == 0 else exit()
-SEG_CELLS_HOP = int(SEG_CELLS/25) if SEG_CELLS % 25 == 0 else exit()
+SEG_CELLS_HOP = int(SEG_CELLS/5) if SEG_CELLS % 5 == 0 else exit()
+
+MEL_HOP = SEG_CELL_LEN
+MEL_N_FFT = 4*MEL_HOP
 
 
 def loadSignal(fileName):
     out, _ = (ffmpeg
               .input(fileName)
-              .output('-', format='s16le', acodec='pcm_s16le', ac=1, ar=SAMPLE_RATE)
+              .output('-', format='f32le', acodec='pcm_f32le', ac=1, ar=SAMPLE_RATE)
               .overwrite_output()
               .run(capture_stdout=True))
-    return np.frombuffer(out, np.int16)
+    return np.frombuffer(out, np.float32)
+
+
+def calcMelSpect(sig):
+    return librosa.feature.melspectrogram(sig, sr=SAMPLE_RATE, n_fft=MEL_N_FFT, hop_length=MEL_HOP).T[:-1]  # it generates one more for some reason
 
 
 def generateDataset():
@@ -37,19 +45,20 @@ def generateDataset():
             # prepare data and targets
             sig = loadSignal(list(DATA_DIR.glob(Path(fileName).stem + ".[!labels]*"))[0])
             sig = sig[:int(len(sig) / SEG_LEN) * SEG_LEN]
-            targets = np.ones(int(sig.shape[0] / SEG_CELL_LEN))
+            spect = calcMelSpect(sig)
+            targets = np.ones(spect.shape[0])
             for (beg, end, tag) in csv.reader(fi, delimiter="\t"):
                 targets[int(float(beg) * SAMPLE_RATE / SEG_CELL_LEN):
                         int(np.ceil(float(end) * SAMPLE_RATE / SEG_CELL_LEN))] = (tag == "r")
 
             # save ranges and labels of segments
             for i in range(0, targets.shape[0] - SEG_CELLS, SEG_CELLS_HOP):
-                dataset["range"].append(np.array([idxOffset + i*SEG_CELL_LEN, idxOffset + (i + SEG_CELLS) * SEG_CELL_LEN]))
+                dataset["range"].append(np.array([idxOffset + i, idxOffset + i + SEG_CELLS]))
                 dataset["label"].append(targets[i:i+SEG_CELLS])
 
             # save data
-            dataset["data"].append(sig)
-            idxOffset += len(sig)
+            dataset["data"].append(spect)
+            idxOffset += spect.shape[0]
 
     dataset["data"] = np.concatenate(dataset["data"], axis=0)
     dataset["range"] = np.array(dataset["range"])
@@ -58,14 +67,14 @@ def generateDataset():
         pickle.dump(dataset, dsFile)
         print("Generated {} samples. Average label = {:.4f}".format(dataset["label"].shape[0], np.mean(dataset["label"])))
 
-    labels = dataset["label"].reshape(-1) == 0
-    ranges = np.where(labels[:-1] != labels[1:])[0] + 1
-    isEven = len(ranges) % 2 == 0
-    if (isEven and labels[0]) or (not isEven and labels[0]):
-        ranges = np.concatenate([[0], ranges])
-    if (isEven and labels[0]) or (not isEven and not labels[0]):
-        ranges = np.concatenate([ranges, [len(labels)]])
-    ranges = ranges.reshape((-1, 2))
+    # labels = dataset["label"].reshape(-1) == 0
+    # ranges = np.where(labels[:-1] != labels[1:])[0] + 1
+    # isEven = len(ranges) % 2 == 0
+    # if (isEven and labels[0]) or (not isEven and labels[0]):
+    #     ranges = np.concatenate([[0], ranges])
+    # if (isEven and labels[0]) or (not isEven and not labels[0]):
+    #     ranges = np.concatenate([ranges, [len(labels)]])
+    # ranges = ranges.reshape((-1, 2))
 
     # with open("outLabels.labels", "w") as file:
     #     for r in ranges * SEG_CELL_LEN / SAMPLE_RATE:
