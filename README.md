5815a9cceee4db5179fea59146dd38a692cdb019
JanDorniak <JanDorniak99@gmail.com>
2021-06-11 00:40:28 +0200

    Modify env creation script and README.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Link to a nice tutorial (start from 10 episode): https://www.youtube.com/playlist?list=PL-wATfeyAMNrtbkCNsLcpoAyBBRJZVlnf
+# SpeechLess
+SpeechLess is a tool that can automatically summarize recordings.
+It is done by detecting and removing redundant fragments such as
+repetitions and prolongations of sounds, syllables, words, or phrases
+as well as silent blocks or sounds that are not words.
+
+## Authors
+- [Paweł Głomski](https://github.com/Exepp)
+- [Tomasz Rusinowicz](https://github.com/axontom)
+- [Jan Dorniak](https://github.com/JanDorniak)
--- makeEnv.sh
+++ makeEnv.sh
@@ -1,8 +1,19 @@
-sudo apt update && \
-    sudo apt install python3.6 && \
-    sudo apt install virtualenv && \
-    sudo apt install ffmpeg && \
-    mkdir venv && \
-    virtualenv -p `which python3.6` ./venv/ && \
-    source venv/bin/activate && \
-    pip install -r requirements.txt
+#!/bin/bash
+if ! command -v conda &> /dev/null
+then
+    echo "Downloading conda"
+    wget "https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh"
+    chmod 777 ./Anaconda3-2021.05-Linux-x86_64.sh
+    ./Anaconda3-2021.05-Linux-x86_64.sh
+    source ~/.bashrc
+fi
+eval "$(conda shell.bash hook)"
+conda create --name slenv python=3.6 ffmpeg && conda activate slenv || exit 1
+
+if [[ "$1" == "gpu" ]]
+then
+    conda install cudnn=7.6.5=cuda10.1_0 tensorflow-gpu=2.2
+else
+    conda install tensorflow=2.2
+fi
+pip install -r requirements.txt
